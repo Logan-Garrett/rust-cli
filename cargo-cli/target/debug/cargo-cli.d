bf27d46b57c056a9d557a83b86f58015d3d81bb1
NineTailSecurity <ninetailsoftware@gmail.com>
2023-10-19 19:01:08 -0400

    Created Cargo For Rust

---
+++ cargo-cli/target/debug/cargo-cli.d
@@ -0,0 +1,1 @@
+/home/ihatewindows/git/rust-cli/cargo-cli/target/debug/cargo-cli: /home/ihatewindows/git/rust-cli/cargo-cli/src/main.rs
---
+++ cargo-cli/target/debug/deps/cargo_cli-273d6e2019109c2f.d
@@ -0,0 +1,5 @@
+/home/ihatewindows/git/rust-cli/cargo-cli/target/debug/deps/cargo_cli-273d6e2019109c2f: src/main.rs
+
+/home/ihatewindows/git/rust-cli/cargo-cli/target/debug/deps/cargo_cli-273d6e2019109c2f.d: src/main.rs
+
+src/main.rs:
