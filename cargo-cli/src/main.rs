9a50b401d96bbba5b65d20a080798e35d47a24a9
NineTailSecurity <ninetailsoftware@gmail.com>
2023-10-19 19:18:59 -0400

    Got an input message lols

--- cargo-cli/src/main.rs
+++ cargo-cli/src/main.rs
@@ -1,3 +1,16 @@
+use std::io::Write;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    	println!("Hello and welcome to 'name'!");
+
+	print!("||[: ");
+	std::io::stdout().flush().unwrap();
+	let mut command = String::new();
+
+	io::stdin()
+		.read_line(&mut command)
+		.expect("Failed to read line");
+
+	println!("Command entered: {command}");
 }
