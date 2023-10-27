4818c47cbcdb51261dfa8dd699943d3db24bdb70
NineTailSecurity <ninetailsoftware@gmail.com>
2023-10-26 23:18:34 -0400

    Some little progress on this massive project

--- cargo-cli/src/main.rs
+++ cargo-cli/src/main.rs
@@ -3,14 +3,19 @@ use std::io;
 
 fn main() {
     	println!("Hello and welcome to 'name'!");
-
-	print!("||[: ");
-	std::io::stdout().flush().unwrap();
+	
 	let mut command = String::new();
 
-	io::stdin()
-		.read_line(&mut command)
-		.expect("Failed to read line");
-
-	println!("Command entered: {command}");
+	while command.trim().to_lowercase() != "exit" {
+		print!("||[: ");
+		std::io::stdout().flush().unwrap();
+		io::stdin()
+			.read_line(&mut command)
+			.expect("Failed to read line");
+		println!("{command}");
+		if command.trim().to_lowercase() == "exit" {
+			break;
+		}
+		command = String::new();
+	}
 }
