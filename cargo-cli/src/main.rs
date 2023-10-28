4100c7686d98b7707482c3fb2f90df2e49265239
NineTailSecurity <ninetailsoftware@gmail.com>
2023-10-28 00:22:37 -0400

    Added first command of many which was pwd

--- cargo-cli/src/main.rs
+++ cargo-cli/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::Write;
 use std::io;
+use std::process::{Command, Output};
 
 fn main() {
     	println!("Hello and welcome to 'name'!");
@@ -12,9 +13,22 @@ fn main() {
 		io::stdin()
 			.read_line(&mut command)
 			.expect("Failed to read line");
-		println!("{command}");
+		print!("{command}");
 		if command.trim().to_lowercase() == "exit" {
 			break;
+		
+		}
+		if command.trim().to_lowercase() == "pwd" {
+			let output: Output = Command::new("pwd")
+			    .output()
+			    .expect("Failed to run command");
+			if output.status.success() {
+				let pwd_output = String::from_utf8_lossy(&output.stdout);
+				
+				print!("Current directory: {}", pwd_output);
+    			} else {
+        			eprintln!("Error running command: {}", String::from_utf8_lossy(&output.stderr));
+    			}
 		}
 		command = String::new();
 	}
